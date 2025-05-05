e20da4a8ac4fb5d64a3c5fb09a1b9a5e3355b91d
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-05 22:52:44 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -65,8 +65,14 @@ vpc_id = aws_vpc.main.id
   }
 }
  
-resource "aws_route_table_association" "public" {
-  subnet_id      = [aws_subnet.public_subnet_1,aws_subnet.public_subnet_2]
-  route_table_id = aws_route_table.public.id
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
 }
- 
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public.id
+}
+
