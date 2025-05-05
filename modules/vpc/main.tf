80d30deb692944c936932dcf653684c3fb1e3c5f
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-05 22:00:16 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,27 +5,27 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-2a"
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-2b"
 }
 
 resource "aws_security_group" "web_sg" {
