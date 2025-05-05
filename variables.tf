a068a3ca5a3b9adbcb6c04980ecedf8591d95c18
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-05 21:45:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,30 +8,6 @@ variable "instance_type" {
   type        = string
 }
 
-variable "public_subnet_1_id" {
-  description = "ID of the first public subnet"
-  type        = string
-}
-
-variable "public_subnet_2_id" {
-  description = "ID of the second public subnet"
-  type        = string
-}
-
-variable "private_subnet_1_id" {
-  description = "ID of the first private subnet"
-  type        = string
-}
-
-variable "private_subnet_2_id" {
-  description = "ID of the second private subnet"
-  type        = string
-}
-
-variable "web_sg_id" {
-  description = "Security group for the web server"
-  type        = string
-}
 
 variable "db_username" {
   description = "Database username"
