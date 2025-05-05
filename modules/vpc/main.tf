66fa6393455209fe506a191243cfd686b763b8e7
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-06 03:58:59 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-2a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "public_subnet_2" {
