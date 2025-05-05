d177a2165d7124f0a785ebfec55794bcd07a8edc
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-05 22:57:16 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,34 +45,33 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_internet_gateway" "gw" {
-vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
   tags = {
     Name = "main-igw"
   }
 }
 
 resource "aws_route_table" "public" {
-vpc_id = aws_vpc.main.id
- 
+  vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.gw.id
   }
- 
+
   tags = {
     Name = "public-rt"
   }
 }
- 
 
 resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public.id
 }
-
