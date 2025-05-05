b82d11c6397a3699051adbc607c6c93ea493b4e2
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-05 21:52:17 +0530

    Create variables.tf

---
+++ modules/web_server/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+  type        = string
+}
+variable "instance_type" {
+  type        = string
+}
+variable "public_subnet_id" {
+  type        = string
+}
+variable "web_sg_id" {
+  type        = string
+}
