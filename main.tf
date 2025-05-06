15892de37b740b6ebbed06af26a14b88c396256c
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-06 14:03:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,6 +91,7 @@ resource "aws_lb_target_group" "openproject_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type="instance"
 }
 
 resource "aws_lb_target_group" "devlake_tg" {
@@ -98,6 +99,7 @@ resource "aws_lb_target_group" "devlake_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type="instance"
 }
 
 # ALB Listener & Rules
