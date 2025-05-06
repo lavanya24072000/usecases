0e6ae5c2f69fc064e95398415192ec2c8a71f6a8
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-06 18:38:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,4 @@
 variable "aws_region" {
   default = "eu-west-1"
 
-variable "instance_type" {
-  default = "t2.medium"
-}
-
-variable "ports" {
-  default = [22, 80, 8080]
-}
-
-variable "tags" {
-  default = {
-    Name        = "OpenProject-DevLake-Setup"
-    Environment = "Dev"
-  }
 }
