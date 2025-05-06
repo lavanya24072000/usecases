ad8b30b0c365fad61a398551ece5b68c8407b42d
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-06 18:43:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,17 @@ variable "ports" {
   type= list(string)
   default = [22, 80, 8080]
 }
+
+
+variable "instance_type" {
+  type= string
+  default = "t2.medium"
+}
+
+variable "tags" {
+  type= string
+  default = {
+    Name        = "OpenProject-DevLake-Setup"
+    Environment = "Dev"
+  }
+}
