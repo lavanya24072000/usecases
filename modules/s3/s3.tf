773521f946bd0202b858f1e8f83fc34b47392928
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-07 23:45:59 +0530

    Update s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,20 +1,20 @@
-Resource “aws_s3_bucket” “source” {
+Resource "aws_s3_bucket" "source" {
   Bucket = var.source_bucket_name
 }
 
-Resource “aws_s3_bucket” “destination” {
+Resource "aws_s3_bucket" "destination" {
   Bucket = var.destination_bucket_name
 }
 
-Output “source_bucket_arn” {
+Output "source_bucket_arn" {
   Value = aws_s3_bucket.source.arn
 }
 
-Output “destination_bucket_arn” {
+Output "destination_bucket_arn" {
   Value = aws_s3_bucket.destination.arn
 }
 
-Output “source_bucket_name” {
+Output "source_bucket_name" {
   Value = aws_s3_bucket.source.id
 }
 
