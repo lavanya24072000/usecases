e85ac7a377aedb00b1c1d09450f28363c4f7171c
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-07 23:54:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable “source_bucket_name” {
-  Default = “lavanya-source-bucket”
+variable "source_bucket_name" {
+  Default = "lavanya-source-bucket"
 }
 
-variable “destination_bucket_name” {
-  Default = “lavanya-destination-bucket”
+variable "destination_bucket_name" {
+  Default = "lavanya-destination-bucket"
 }
 
-variable “sns_topic_name” {
-  Default = “image-resize-topic”
+variable "sns_topic_name" {
+  Default = "image-resize-topic"
 }
