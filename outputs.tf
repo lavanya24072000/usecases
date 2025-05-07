86f946ae688b2868b46954025354d36947c56bf3
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-07 23:54:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output “source_bucket” {
+output "source_bucket" {
   value = module.s3.source_bucket_name
 }
 
-output “lambda_function_name” {
+output "lambda_function_name" {
   value = module.lambda.lambda_function_name
 }
 
