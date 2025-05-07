064ed96d2a1a1345889bfb7ece12325f6e65ca97
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-07 23:50:14 +0530

    Update lambda.tf

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,44 +1,44 @@
-Data "archive_file" "lambda_zip" {
-  Type        = "zip"
-  Source_dir  = "${path.module}/lambda_src"
-  Output_path = "${path.module}/lambda.zip"
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_src"
+  output_path = "${path.module}/lambda.zip"
 }
 
-Resource "aws_lambda_function" "resize_image" {
-  Filename         = data.archive_file.lambda_zip.output_path
-  Function_name    = "resize-image"
-  Role             = var.lambda_role_arn
-  Handler          = "index.lambda_handler"
-  Runtime          = "python3.9"
-  Source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+resource "aws_lambda_function" "resize_image" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "resize-image"
+  role             = var.lambda_role_arn
+  handler          = "index.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
-  Environment {
-    Variables = {
+  environment {
+    variables = {
       DEST_BUCKET   = var.destination_bucket
       SNS_TOPIC_ARN = var.sns_topic_arn
     }
   }
 }
 
-Resource "aws_lambda_permission" "s3_invoke" {
-  Statement_id  = "AllowExecutionFromS3"
-  Action        = "lambda:InvokeFunction"
-  Function_name = aws_lambda_function.resize_image.arn
-  Principal     = "s3.amazonaws.com"
-  Source_arn    = "arn:aws:s3:::${var.source_bucket}"
+resource "aws_lambda_permission" "s3_invoke" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.resize_image.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.source_bucket}"
 }
 
-Resource "aws_s3_bucket_notification" "trigger_lambda" {
-  Bucket = var.source_bucket
+resource "aws_s3_bucket_notification" "trigger_lambda" {
+  bucket = var.source_bucket
 
-  Lambda_function {
-    Lambda_function_arn = aws_lambda_function.resize_image.arn
-    Events              = ["s3:ObjectCreated:*"]
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.resize_image.arn
+    events              = ["s3:ObjectCreated:*"]
   }
 
-  Depends_on = [aws_lambda_permission.s3_invoke]
+  depends_on = [aws_lambda_permission.s3_invoke]
 }
 
-Output "lambda_function_name" {
-  Value = aws_lambda_function.resize_image.function_name
+output "lambda_function_name" {
+  value = aws_lambda_function.resize_image.function_name
 }
