e7938c20f0d06bf7d72f04532af2e88804f9d090
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-07 23:44:15 +0530

    Update iam.tf

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,46 +1,46 @@
 
-Resource “aws_iam_role” “lambda_exec” {
-  Name = “lambda_exec_role”
+Resource "aws_iam_role" "lambda_exec" {
+  Name = "lambda_exec_role"
   Assume_role_policy = jsonencode({
-    Version = “2012-10-17”,
+    Version = "2012-10-17",
     Statement = [{
-      Effect = “Allow”,
+      Effect = "Allow",
       Principal = {
-        Service = “lambda.amazonaws.com”
+        Service = "lambda.amazonaws.com"
       },
-      Action = “sts:AssumeRole”
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
-Resource “aws_iam_role_policy” “lambda_policy” {
-  Name = “lambda_policy”
+Resource "aws_iam_role_policy" "lambda_policy" {
+  Name = "lambda_policy"
   Role = aws_iam_role.lambda_exec.id
   Policy = jsonencode({
-    Version = “2012-10-17”,
+    Version = "2012-10-17",
     Statement = [
       {
         Action = [
-          “s3:GetObject”,
-          “s3:PutObject”
+          "s3:GetObject",
+          "s3:PutObject"
         ],
-        Effect   = “Allow”,
-        Resource = [“${var.source_bucket_arn}/*”, “${var.destination_bucket_arn}/*”]
+        Effect   = "Allow",
+        Resource = ["${var.source_bucket_arn}/*", "${var.destination_bucket_arn}/*"]
       },
       {
-        Action = [“sns:Publish”],
-        Effect = “Allow”,
+        Action = ["sns:Publish"],
+        Effect = "Allow",
         Resource = var.sns_topic_arn
       },
       {
-        Action = [“logs:*”],
-        Effect = “Allow”,
-        Resource = “*”
+        Action = ["logs:*"],
+        Effect = "Allow",
+        Resource = "*"
       }
     ]
   })
 }
 
-Output “lambda_role_arn” {
+Output "lambda_role_arn" {
   Value = aws_iam_role.lambda_exec.arn
 }
