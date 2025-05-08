be376f812e4b842c832fbaeee37e49fcb0cb14cb
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-08 12:50:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pavithra-bucket001"
+    bucket         = "lavanya-bucket001"
     key            = "uc5/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
