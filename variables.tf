a6b290173f39e6eb70e012a1400cdc5fc08e89a8
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-08 09:50:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,17 +9,4 @@ variable "destination_bucket_name" {
 variable "sns_topic_name" {
   default = "image-resize-topic"
 }
-variable "source_bucket_arn" {
-  description = "ARN of the source S3 bucket"
-  type        = string
-}
-
-variable "destination_bucket_arn" {
-  description = "ARN of the destination S3 bucket"
-  type        = string
-}
-
-variable "sns_topic_arn" {
-  description = "ARN of the SNS topic"
-  type        = string
-}
+    
