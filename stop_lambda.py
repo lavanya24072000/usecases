955ceb6dff5f069037b16a2e1ce55d6f0989642a
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-09 18:53:36 +0000

    first

--- stop_lambda.py
+++ stop_lambda.py
@@ -1,8 +1,26 @@
 import boto3
 import os
-
+ 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
-    instance_ids = os.environ['INSTANCE_IDS'].split(',')
-    ec2.stop_instances(InstanceIds=instance_ids)
-    print(f"Stopped instances: {instance_ids}")
+    instance_id = os.environ.get('INSTANCE_ID')
+ 
+    if not instance_id:
+        return {
+            'statusCode': 400,
+            'body': 'Environment variable INSTANCE_ID is not set'
+        }
+ 
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id])
+        return {
+            'statusCode': 200,
+            'body': f'Successfully stopped instance {instance_id}'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error stopping instance: {str(e)}'
+        }
+ 
+ 
