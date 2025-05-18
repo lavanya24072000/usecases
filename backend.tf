2bd27d348deb6d8e5b1bc498b7275f7922121d10
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-18 11:03:30 +0000

    pushsdsds

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "lavanya-bucket001"
-    key            = "uc6/terraform.tfstate"
+    bucket         = "demostore001"
+    key            = "uc5/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true
