de51522f8ac003e4a7f19db5e3e1b390452c0e9f
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-18 23:46:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,29 @@
 variable "region" {
-  default = "us-west-2"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "environment" {
-  default = "dev"
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "azs" {
-  type    = list(string)
-  default = ["us-west-2a", "us-west-2b"]
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
 }
