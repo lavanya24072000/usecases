26de9b4041b0f5291666fc42bf02c22f9b016b24
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-18 23:46:30 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,29 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
 }
 
 output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
+  description = "EKS Cluster name"
+  value       = aws_eks_cluster.eks.name
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.flask_app.repository_url
+output "eks_cluster_endpoint" {
+  description = "EKS Cluster endpoint"
+  value       = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_security_group" {
+  description = "Security group associated with the EKS cluster"
+  value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
+
+output "alb_security_group_id" {
+  description = "Security Group ID for the ALB"
+  value       = aws_security_group.alb_sg.id
 }
