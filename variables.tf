0efa3bf5e5f52a7124947089a431a85cebbae307
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 12:03:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,15 @@
-variable "source_bucket_name" { default = "source-bucket-image-lavanya" }
-variable "dest_bucket_name"   { default = "dest-bucket-image-lavanya" }
-variable "sns_topic_name"     { default = "image-topic" }
-variable "lambda_function_name" { default = "lambda-image" }
-variable "resize_width" { default = 600 }
+variable "aws_region" {
+  default = "us-east-1"
+}
  
-variable "tags" {
-  type = map(string)
-  default = {
-    Project   = "ImageProcessor"
-    Owner     = "prodTeam"
-    ManagedBy = "Terraform"
-  }
+variable "ami_id" {
+  default = "ami-0c7217cdde317cfec"
 }
-variable "email" { 
-  default = "elavanya@hcltech.com"
+ 
+variable "instance_type" {
+  default = "t2.micro"
 }
-variable "lambda_role" { 
-  default = "lambda_role_s3_image"
+ 
+variable "key_name" {
+  default = "your-key-name"
 }
