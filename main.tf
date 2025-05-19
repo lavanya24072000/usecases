b31e4f3d9fa4eb15b941ed2dd21e9654c6ad92b3
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:37:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region = var.aws_region
 }
- 
+
 module "vpc" {
   source = "./modules/vpc"
 }
- 
+
 module "ec2" {
   source         = "./modules/ec2"
   ami_id         = var.ami_id
@@ -14,11 +14,11 @@ module "ec2" {
   vpc_id         = module.vpc.vpc_id
   subnet_id      = module.vpc.subnet_id
 }
- 
+
 module "alb" {
   source            = "./modules/alb"
   vpc_id            = module.vpc.vpc_id
-  subnet_id         = module.vpc.public_subnet_ids  
+  subnet_id         = module.vpc.public_subnet_ids
   ec2_id            = module.ec2.ec2_id
   security_group_id = module.ec2.security_group_id
 }
