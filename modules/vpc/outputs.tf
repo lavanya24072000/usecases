7c818bf583bcaeb7498a658cca2ff6a2561c9ad0
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:08:09 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,7 +1,19 @@
 output "vpc_id" {
-value = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
- 
-output "subnet_id" {
-value = aws_subnet.public.id
+
+output "public_subnet_a_id" {
+  description = "The ID of the public subnet in AZ a"
+  value       = aws_subnet.public_a.id
+}
+
+output "public_subnet_b_id" {
+  description = "The ID of the public subnet in AZ b"
+  value       = aws_subnet.public_b.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
