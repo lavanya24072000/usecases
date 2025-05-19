3393c29185ee53837f96e5c4b767cf9ba85eb814
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:34:08 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,42 +1,33 @@
 resource "aws_vpc" "main" {
-cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = { Name = "focalboard-vpc" }
 }
- 
 
 resource "aws_subnet" "public_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-  tags = { Name = "focalboard-public-subnet-a" }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = { Name = "focalboard-public-subnet-a" }
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1b"
-  tags = { Name = "focalboard-public-subnet-b" }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = { Name = "focalboard-public-subnet-b" }
 }
-
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_b" {
-  
 
 resource "aws_internet_gateway" "igw" {
-vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
   tags = { Name = "focalboard-igw" }
 }
- 
+
 resource "aws_route_table" "public" {
-vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -44,4 +35,12 @@ vpc_id = aws_vpc.main.id
   tags = { Name = "focalboard-public-rt" }
 }
 
- 
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
