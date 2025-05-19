92709e564fa596aeba5740c3662cf86ab16fb5d0
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 17:53:00 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -27,10 +27,7 @@ resource "aws_security_group" "focal_sg" {
  
 
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "first"
-  public_key = file(var.public_key_path)
-}
+
 
 resource "aws_instance" "focal_ec2" {
   ami                         = var.ami_id
@@ -38,8 +35,7 @@ resource "aws_instance" "focal_ec2" {
   subnet_id                   = var.subnet_ids[0]
   vpc_security_group_ids      = [aws_security_group.focal_sg.id]
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.deployer.key_name
-
+  key_name                    = first
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
