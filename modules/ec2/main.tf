0b7d260dfa29a5a7b9be2ed09aac61244ef5727d
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 12:39:26 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -29,11 +29,18 @@ resource "aws_instance" "focal_ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-vpc_security_group_ids = [aws_security_group.focal_sg.id]
+  vpc_security_group_ids = [aws_security_group.focal_sg.id]
   associate_public_ip_address = true
   key_name               = var.key_name
  
-user_data = file("${path.module}/../../user_data.sh")
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 8000:8000 mattermost/focalboard
+              EOF
  
   tags = { Name = "focalboard-instance" }
 }
