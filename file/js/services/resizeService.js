45ca7523c995de510baa895729d1b38b8c8de4b6
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 12:04:11 +0530

    Delete file/js directory

--- file/js/services/resizeService.js
+++
@@ -1,8 +0,0 @@
-const sharp = require('sharp');
- 
-exports.resizeImage = async (imageBuffer) => {
-  return await sharp(imageBuffer)
-    .resize(300, 300)  // Resize to 300x300 pixels (change as needed)
-    .toFormat('jpeg')
-    .toBuffer();
-};
