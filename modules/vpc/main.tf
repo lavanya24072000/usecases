d3c31b36e1713685a41d8ea68fa21418a6e6bbb2
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:03:32 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,14 +5,31 @@ cidr_block = "10.0.0.0/16"
   tags = { Name = "focalboard-vpc" }
 }
  
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-  tags = { Name = "focalboard-public-subnet" }
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = { Name = "focalboard-public-subnet-a" }
 }
- 
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = { Name = "focalboard-public-subnet-b" }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  
+
 resource "aws_internet_gateway" "igw" {
 vpc_id = aws_vpc.main.id
   tags = { Name = "focalboard-igw" }
@@ -26,10 +43,6 @@ vpc_id = aws_vpc.main.id
   }
   tags = { Name = "focalboard-public-rt" }
 }
- 
-resource "aws_route_table_association" "public" {
-subnet_id = aws_subnet.public.id
-route_table_id = aws_route_table.public.id
-}
+
  
 
