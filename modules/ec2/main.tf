a29baeeb10e512f216ac9cdb5bea4b215e0f94d2
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 17:57:09 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "focal_ec2" {
   subnet_id                   = var.subnet_ids[0]
   vpc_security_group_ids      = [aws_security_group.focal_sg.id]
   associate_public_ip_address = true
-  key_name                    = first
+  key_name                    = "first"
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
