d7257ec8aecde5eae303c82e918203bb815682fe
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:36:23 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,9 +1,10 @@
 variable "vpc_id" {}
 
 variable "subnet_ids" {
-  type  = list(string)
-  description = "List of subnet IDs in different Availability Zones"
+  type        = list(string)
+  description = "List of subnet IDs in different Availability Zones"
 }
 
 variable "ec2_id" {}
+
 variable "security_group_id" {}
