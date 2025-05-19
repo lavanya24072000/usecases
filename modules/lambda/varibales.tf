214ed030edd8e5184496f1cbe1a872e3340f3b25
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 12:00:31 +0530

    Delete modules/lambda directory

--- modules/lambda/varibales.tf
+++
@@ -1,7 +0,0 @@
-variable "function_name" {}
-variable "role_arn" {}
-variable "dest_bucket_name" {}
-variable "sns_topic_arn" {}
-variable "resize_width" {}
-variable "tags" {}
-variable "source_bucket_name" {}
