a3f5ca51904916a4205295a3c0253d0430112e29
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:47:56 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,21 +2,21 @@ resource "aws_security_group" "focal_sg" {
   name        = "focalboard-sg"
   description = "Allow HTTP and SSH"
   vpc_id      = var.vpc_id
- 
+
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -24,23 +24,21 @@ resource "aws_security_group" "focal_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
- 
+
 resource "aws_instance" "focal_ec2" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
- subnet_id               = var.subnet_ids[0]
-  vpc_security_group_ids = [aws_security_group.focal_sg.id]
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_ids[0]
+  vpc_security_group_ids      = [aws_security_group.focal_sg.id]
   associate_public_ip_address = true
-  key_name               = var.key_name
- 
+  key_name                    = var.key_name
+
   user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y docker.io
-              systemctl start docker
-              systemctl enable docker
+                           systemctl enable docker
               docker run -d -p 8000:8000 mattermost/focalboard
               EOF
- 
-  tags = { Name = "focalboard-instance" }
+
+  tags = {
+    Name = "focalboard-instance"
+  }
 }
