4da0c1c1da2dfa6b3e428b81dfdcac24284b709a
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:35:32 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,18 +1,17 @@
 resource "aws_lb" "focal_alb" {
-  name               = "focalboard-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [var.security_group_id]
-  subnets            = var.subnet_ids  
+  name               = "focalboard-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = var.subnet_ids
 }
 
- 
 resource "aws_lb_target_group" "focal_tg" {
   name     = "focalboard-tg"
   port     = 8000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
- 
+
   health_check {
     path                = "/"
     protocol            = "HTTP"
@@ -23,17 +22,18 @@ resource "aws_lb_target_group" "focal_tg" {
     unhealthy_threshold = 2
   }
 }
- 
+
 resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.focal_tg.arn
   target_id        = var.ec2_id
   port             = 8000
 }
- 
+
 resource "aws_lb_listener" "focal_listener" {
   load_balancer_arn = aws_lb.focal_alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.focal_tg.arn
