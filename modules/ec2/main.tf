0c50b38e29096a880cea2d43b5227bda574a90e4
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 13:46:53 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "focal_sg" {
 resource "aws_instance" "focal_ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.subnet_ids
+ subnet_id               = var.subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.focal_sg.id]
   associate_public_ip_address = true
   key_name               = var.key_name
