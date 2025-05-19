a2685d3785ed2079ee43798b4340038d9e434317
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 16:26:14 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -5,5 +5,7 @@ variable "subnet_ids" {}
 variable "vpc_id" {}
 
 variable "public_key_path" {
-  default = "/home/ubuntu/.ssh/id_rsa.pub"
+  description = "Path to the SSH public key"
+  type        = string
+  default     = "/home/ubuntu/.ssh/id_rsa.pub"
 }
