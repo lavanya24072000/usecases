309ffa03e33bd257878f0997177cf7b17ad0ba7d
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-19 12:58:42 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,10 +6,10 @@ cidr_block = "10.0.0.0/16"
 }
  
 resource "aws_subnet" "public" {
-vpc_id = aws_vpc.main.id
-cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = us-east-1a
+  availability_zone       = "us-east-1a"
   tags = { Name = "focalboard-public-subnet" }
 }
  
