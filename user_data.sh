cd19910acaf72c4922499509b671f1f2609a902a
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-20 11:57:59 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-yum update -y
-yum install python3 git -y
+apt update -y
+apt install -y python3 python3-pip git
+
 pip3 install flask boto3 pymysql
- 
-cd /home/ec2-user
-cat < app.py
+
+cd /home/ubuntu
+
+cat > app.py << 'EOF'
 from flask import Flask
 import boto3, json, pymysql
- 
+
 app = Flask(__name__)
- 
+
 def get_secret():
     client = boto3.client('secretsmanager', region_name='us-east-1')
-    secret = json.loads(client.get_secret_value(SecretId='aurora-db-secret-first')['SecretString'])
+    secret = json.loads(client.get_secret_value(SecretId='aurora-db-secret')['SecretString'])
     return secret
- 
+
 @app.route('/')
 def index():
     secret = get_secret()
@@ -28,10 +30,9 @@ def index():
         return "Connected to AuroraDB successfully!"
     except Exception as e:
         return str(e)
- 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 EOF
- 
+
 nohup python3 app.py &
- 
