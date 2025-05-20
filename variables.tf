8e67d761f2b4cc74430aa6603ec9afccac20d1b1
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-20 10:56:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
 
-variable "region" {{
+variable "region" {
   description = "AWS region"
-}}
+}
 
-variable "db_username" {{
+variable "db_username" {
   description = "Database username"
-}}
+}
 
-variable "db_password" {{
+variable "db_password" {
   description = "Database password"
-}}
+}
 
-variable "key_name" {{
+variable "key_name" {
   description = "Key pair name for EC2 instance"
-}}
+}
 
-variable "security_group_id" {{
+variable "security_group_id" {
   description = "ID of the existing security group in the correct VPC"
-}}
+}
 
-variable "subnet_id_1" {{
+variable "subnet_id_1" {
   description = "First subnet ID for RDS (in one AZ)"
-}}
+}
 
-variable "subnet_id_2" {{
+variable "subnet_id_2" {
   description = "Second subnet ID for RDS (in a different AZ)"
-}}
+}
 
-variable "ec2_subnet_id" {{
+variable "ec2_subnet_id" {
   description = "Subnet ID for EC2 instance"
-}}
+}
