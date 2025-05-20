0b0440aa728d613131e31d20d71c1dcdef93e2ce
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-20 10:26:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,27 @@
-variable "region"           { default = "us-east-1" }
-variable "vpc_id"           {}
-variable "subnet_id"        {}
-variable "security_group_id" {}
-variable "db_username"      { type = string }
-variable "db_password"      { type = string }
-variable "key_name"         {}  #
+variable "region"  {
+   default = "us-east-1" 
+}
+variable "vpc_id"           {
+   type = string 
+   default = "vpc-06c1c352d16a03fe2"
+}
+variable "subnet_id"        {
+   type = string 
+   default = "subnet-029a8beb2b79e585c"
+}
+variable "security_group_id" {
+   type = string 
+   default = "sg-0465db0ccfd942129"
+}
+variable "db_username"      {
+   type = string 
+   default = "lavanya"
+}
+variable "db_password"      { 
+   type = string 
+   default = "Lavanya@2407"
+}
+variable "key_name"         {
+   type = string 
+   default = "first"
+}  
