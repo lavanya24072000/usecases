6d2c40c79fedf1e3eea52170a0548e55f9f4db23
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-20 10:49:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,32 @@
-variable "region"  {
-   default = "us-east-1" 
-}
-variable "vpc_id"           {
-   type = string 
-   default = "vpc-06c1c352d16a03fe2"
-}
-variable "subnet_id"        {
-   type = string 
-   default = "subnet-029a8beb2b79e585c"
-}
-variable "security_group_id" {
-   type = string 
-   default = "sg-0465db0ccfd942129"
-}
-variable "db_username"      {
-   type = string 
-   default = "lavanya"
-}
-variable "db_password"      { 
-   type = string 
-   default = "Lavanya@2407"
-}
-variable "key_name"         {
-   type = string 
-   default = "first"
-}  
+
+variable "region" {{
+  description = "AWS region"
+}}
+
+variable "db_username" {{
+  description = "Database username"
+}}
+
+variable "db_password" {{
+  description = "Database password"
+}}
+
+variable "key_name" {{
+  description = "Key pair name for EC2 instance"
+}}
+
+variable "security_group_id" {{
+  description = "ID of the existing security group in the correct VPC"
+}}
+
+variable "subnet_id_1" {{
+  description = "First subnet ID for RDS (in one AZ)"
+}}
+
+variable "subnet_id_2" {{
+  description = "Second subnet ID for RDS (in a different AZ)"
+}}
+
+variable "ec2_subnet_id" {{
+  description = "Subnet ID for EC2 instance"
+}}
