fb6bc0b30445f974e4fc3f8ad79c4775612aa2d7
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-23 15:41:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,28 @@
-output "cognito_login_url" {
-value = "https://${aws_cognito_user_pool_client.user_pool_client.user_pool_id}.auth.${var.region}.amazoncognito.com/login"
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
+
+output "api_id" {
+  value = aws_apigatewayv2_api.http_api.id
+}
+output "user_pool_id" {
+  value = aws_cognito_user_pool.this.id
+}
+
+output "user_pool_arn" {
+  value = aws_cognito_user_pool.this.arn
+}
+
+output "client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
+output "role_arn" {
+  value = aws_iam_role.lambda_exec_role.arn
+}
+output "lambda_name" {
+  value = aws_lambda_function.this.function_name
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.this.arn
 }
