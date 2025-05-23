c59ec03ca0971ba2ff23b2023d1427f0c3d3588f
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-23 15:39:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,43 @@
-provider "aws" {
-  region = var.region
-}
- 
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-      Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
- 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-role = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "hello_api"
+  protocol_type = "HTTP"
 }
 
- data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/lambda"
-  output_path = "${path.module}/lambda.zip"
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = var.lambda_function_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
 }
 
-resource "aws_lambda_function" "hello" {
-  function_name = "hello-world"
-  handler       = "main.lambda_handler"
-  runtime       = "python3.12"
-  role          = aws_iam_role.lambda_exec.arn
-  filename = data.archive_file.lambda_zip.output_path
-  source_code_hash =data.archive_file.lambda_zip.output_base64sha256
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
-resource "aws_cognito_user_pool" "user_pool" {
-  name = "hello-user-pool"
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
 }
- resource "aws_cognito_user_pool_client" "client" {
-  name         = "first"
-  user_pool_id = aws_cognito_user_pool.user_pool.id
- 
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+resource "aws_cognito_user_pool" "this" {
+  name = var.user_pool_name
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = var.app_client_name
+  user_pool_id = aws_cognito_user_pool.this.id
+
   generate_secret     = false
   allowed_oauth_flows = ["code"]
   allowed_oauth_scopes = [
@@ -51,71 +47,54 @@ resource "aws_cognito_user_pool" "user_pool" {
     "aws.cognito.signin.user.admin"
   ]
   supported_identity_providers = ["COGNITO"]
- 
+
   callback_urls = [
-    "https://example.amazonaws.com"
+    "https://${var.api_id}.execute-api.${var.aws_region}.amazonaws.com"
   ]
- 
+
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
- 
+
   allowed_oauth_flows_user_pool_client = true
 }
- 
- 
+
+
 resource "aws_cognito_user_pool_domain" "domain" {
-  domain       = "test"
-  user_pool_id = aws_cognito_user_pool.user_pool.id
+  domain       = var.domain_prefix
+  user_pool_id = aws_cognito_user_pool.this.id
+}
+resource "aws_iam_role" "lambda_exec_role" {
+  name = var.lambda_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
 
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "${var.lambda_role_name}-policy-attach"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+resource "aws_lambda_function" "this" {
+  filename         = var.lambda_zip_path
+  function_name    = var.function_name
+  role          = var.role_arn
+  handler          = var.handler
+  runtime          = var.runtime
+  source_code_hash = filebase64sha256(var.lambda_zip_path)
+  timeout          = 60
 
-resource "aws_api_gateway_rest_api" "api" {
-  name = "hello-api"
-}
- 
-resource "aws_api_gateway_resource" "hello_resource" {
-rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "hello"
-}
- 
-resource "aws_api_gateway_method" "get_hello" {
-rest_api_id = aws_api_gateway_rest_api.api.id
-resource_id = aws_api_gateway_resource.hello_resource.id
-  http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
-authorizer_id = aws_api_gateway_authorizer.auth.id
-}
- 
-resource "aws_api_gateway_authorizer" "auth" {
-  name               = "cognito-authorizer"
-rest_api_id = aws_api_gateway_rest_api.api.id
-  identity_source    = "method.request.header.Authorization"
-  type               = "COGNITO_USER_POOLS"
-  provider_arns      = [aws_cognito_user_pool.user_pool.arn]
-}
- 
-resource "aws_api_gateway_integration" "lambda_integration" {
-rest_api_id = aws_api_gateway_rest_api.api.id
-resource_id = aws_api_gateway_resource.hello_resource.id
-  http_method             = aws_api_gateway_method.get_hello.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.hello.invoke_arn
-}
- 
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello.function_name
-  principal = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
- 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_integration]
-rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
+  environment {
+    variables = var.environment_variables
+  }
 }
