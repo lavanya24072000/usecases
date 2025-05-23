10aa88b5f7ec68d7b9874f587e80d9d398f91114
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-23 15:40:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,28 @@
-variable "region" {
+variable "lambda_zip_path" {}
+variable "function_name" {}
+variable "handler" {
+  default = "lambda_function.lambda_handler"
+}
+variable "runtime" {
+  default = "python3.8"
+}
+variable "environment_variables" {
+  type    = map(string)
+  default = {}
+}
+variable "role_arn" {}
+variable "lambda_role_name" {}
+variable "user_pool_name" {}
+variable "app_client_name" {}
+variable "domain_prefix" {}
+
+variable "api_id" {
+  description = "API Gateway ID for callback URL"
+  type        = string
+}
+
+variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
+variable "lambda_function_arn" {}
