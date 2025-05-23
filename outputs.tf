81bd9513842c0700d76fc3067045d1e03f3ee3a3
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-23 14:51:06 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "api_url" {
-  description = "API Gateway Invoke URL"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/hello"
-}
- 
 output "cognito_login_url" {
 value = "https://${aws_cognito_user_pool_client.user_pool_client.user_pool_id}.auth.${var.region}.amazoncognito.com/login"
 }
