fcee9ca0613f60d5ed8a9cbab8e849889ad4e726
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-24 20:35:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,101 +1,62 @@
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "hello_api"
-  protocol_type = "HTTP"
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.this.arn
-
-  integration_method = "POST"
-  payload_format_version = "2.0"
+resource "aws_s3_bucket" "documents_bucket" {
+  bucket = "semantic-search-documents"
 }
 
-resource "aws_apigatewayv2_route" "default_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_stage" "default_stage" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
-resource "aws_lambda_permission" "allow_apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name =  aws_lambda_function.this.arn
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
-}
-resource "aws_cognito_user_pool" "this" {
-  name = var.user_pool_name
-}
-
-resource "aws_cognito_user_pool_client" "client" {
-  name         = var.app_client_name
-  user_pool_id = aws_cognito_user_pool.this.id
-
-  generate_secret     = false
-  allowed_oauth_flows = ["code"]
-  allowed_oauth_scopes = [
-    "openid",
-    "email",
-    "profile",
-    "aws.cognito.signin.user.admin"
-  ]
-  supported_identity_providers = ["COGNITO"]
-
-  callback_urls = [
-    "https://example.amazonaws.com"
-  ]
-
-  explicit_auth_flows = [
-    "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH"
-  ]
-
-  allowed_oauth_flows_user_pool_client = true
-}
-
-
-resource "aws_cognito_user_pool_domain" "domain" {
-  domain       = var.domain_prefix
-  user_pool_id = aws_cognito_user_pool.this.id
-}
-resource "aws_iam_role" "lambda_exec_role" {
-  name = var.lambda_role_name
-
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Sid    = "",
       Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
+        Service = "lambda.amazonaws.com",
+      },
+    }],
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_basic_execution" {
-  name       = "${var.lambda_role_name}-policy-attach"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
 }
-resource "aws_lambda_function" "this" {
-  filename         = var.lambda_zip_path
-  function_name    = var.function_name
-  role          =   aws_iam_role.lambda_exec_role.arn
-  handler          = var.handler
-  runtime          = var.runtime
-  source_code_hash = filebase64sha256(var.lambda_zip_path)
-  timeout          = 60
+
+resource "aws_lambda_function" "ingestion_function" {
+  function_name = "document_ingestion"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  source_code_hash = filebase64sha256("${path.module}/lambda/ingestion.zip")
 
   environment {
-    variables = var.environment_variables
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.documents_bucket.bucket
+      DB_HOST     = var.db_host
+      DB_NAME     = var.db_name
+      DB_USER     = var.db_user
+      DB_PASSWORD = var.db_password
+    }
   }
+
+  s3_bucket = aws_s3_bucket.documents_bucket.bucket
+}
+
+resource "aws_rds_cluster" "aurora_cluster" {
+  cluster_identifier = "aurora-cluster"
+  engine             = "aurora-postgresql"
+  master_username    = var.db_user
+  master_password    = var.db_password
+  skip_final_snapshot = true
+}
+
+resource "aws_rds_cluster_instance" "aurora_instance" {
+  count              = 1
+  identifier         = "aurora-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = "db.r5.large"
 }
