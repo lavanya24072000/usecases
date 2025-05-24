dc3175a8532f7d47c6ec0b2922625fb62aff54c3
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-24 21:14:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,29 @@
 variable "db_host" {
   type = string
+  default= "first"
 }
 
 variable "db_name" {
   type = string
+  default= "first"
 }
 
 variable "db_user" {
   type = string
+  default= "first"
 }
 
 variable "db_password" {
   type = string
+  default= "Prashola@2407"
 }
 
 variable "vpc_id" {
   type = string
+  default= "vpc-06c1c352d16a03fe2"
 }
 
 variable "subnet_ids" {
   type = list(string)
+  default= "subnet-029a8beb2b79e585"
 }
