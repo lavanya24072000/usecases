246d2850e7c9a8412b7410b1edce04135810ddda
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-24 22:32:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "documents_bucket" {
   bucket = "semantic-search-documents"
 }
@@ -10,30 +14,26 @@ resource "aws_iam_role" "lambda_role" {
   name = "lambda-role-${random_id.suffix.hex}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Sid    = "",
-      Principal = {
-        Service = "lambda.amazonaws.com",
-      },
-    }],
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Sid    = "",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
   })
 }
-
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
 }
 
-
 resource "aws_lambda_function" "ingestion_function" {
-  function_name = "document_ingestion"  
+  function_name = "document_ingestion"
   s3_bucket     = "my-cognito-login-page"
   s3_key        = "ingestion.zip"
   role          = aws_iam_role.lambda_role.arn
@@ -54,21 +54,18 @@ resource "aws_lambda_function" "ingestion_function" {
   }
 }
 
-
-
-
 resource "aws_rds_cluster" "aurora_cluster" {
-  cluster_identifier = "aurora-cluster"
-  engine             = "aurora-postgresql"
-  master_username    = var.db_user
-  master_password    = var.db_password
-  skip_final_snapshot = true
+  cluster_identifier   = "aurora-cluster"
+  engine               = "aurora-postgresql"
+  master_username      = var.db_user
+  master_password      = var.db_password
+  skip_final_snapshot  = true
 }
 
 resource "aws_rds_cluster_instance" "aurora_instance" {
-  count              = 1
-  identifier         = "aurora-instance-${count.index}"
-  cluster_identifier = aws_rds_cluster.aurora_cluster.id
-  instance_class     = "db.r5.large"
-  engine             = "aurora-postgresql"
+  count               = 1
+  identifier          = "aurora-instance-${count.index}"
+  cluster_identifier  = aws_rds_cluster.aurora_cluster.id
+  instance_class      = "db.r5.large"
+  engine              = "aurora-postgresql"
 }
