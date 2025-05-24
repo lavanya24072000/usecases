ecf5d871829c1aca940e4d10d7fedba3e54a6f42
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-24 20:36:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,43 +1,23 @@
-variable "lambda_zip_path" {
-  default = "./lambda_function.zip"
+variable "db_host" {
+  type = string
 }
 
-variable "function_name" {
-  default = "lambda_hello"
+variable "db_name" {
+  type = string
 }
 
-variable "handler" {
-  default = "lambda_function.lambda_handler"
+variable "db_user" {
+  type = string
 }
 
-variable "runtime" {
-  default = "python3.8"
+variable "db_password" {
+  type = string
 }
 
-variable "environment_variables" {
-  type    = map(string)
-  default = {}
+variable "vpc_id" {
+  type = string
 }
 
-variable "lambda_role_name" {
-  default = "lambda_role"
-}
-
-variable "user_pool_name" {
-  default = "hello_user_pool"
-}
-
-variable "app_client_name" {
-  default = "hello_app_client"
-}
-
-variable "domain_prefix" {
-  default = "firstexample"
-}
-
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+variable "subnet_ids" {
+  type = list(string)
 }
