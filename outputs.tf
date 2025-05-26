37decdaac7babb96930c45aa0f3e719deeb7ba36
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-26 13:05:23 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,16 @@
- variable "aws_region" {
-  default = "us-east-1"
+output "s3_bucket_name" {
+  value = aws_s3_bucket.documents_bucket.bucket
+}
+ 
+output "db_address" {
+  value = aws_db_instance.pg.address
+}
+ 
+output "lambda_function_name" {
+  value = aws_lambda_function.search_api.function_name
+}
+ 
+output "api_gateway_url" {
+  value = "${aws_api_gateway_rest_api.semantic_api.execution_arn}/search"
 }
  
