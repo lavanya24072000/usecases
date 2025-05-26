04cfe825b43157a35d9fdf14f544fe45bafe508e
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-26 23:14:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,24 @@ provider "aws" {
   region = var.aws_region
 }
 
+
 resource "aws_cloudtrail" "trail" {
-  name                          = var.cloudtrail_name
-  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_logging                = true
+  name                          = var.cloudtrail_name
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
 
-  event_selector {
-    read_write_type           = "All"
-    include_management_events = true
-  }
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail_log_group.arn
+  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_role.arn
 
-  cloud_watch_logs_group_arn  = aws_cloudwatch_log_group.cloudtrail_log_group.arn
-  cloud_watch_logs_role_arn   = aws_iam_role.cloudtrail_role.arn
+  depends_on = [
+    aws_cloudwatch_log_group.cloudtrail_log_group,
+    aws_iam_role.cloudtrail_role,
+    aws_iam_role_policy_attachment.cloudtrail_policy_attachment
+  ]
 }
+
 
 resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket = var.cloudtrail_bucket_name
