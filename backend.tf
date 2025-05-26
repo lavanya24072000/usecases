007d7f1cfba142ef28aa5dfe67b2cbecc049f862
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-26 23:03:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "abhi100bucket121"
-    key            = "usecase15/terraform.tfstate"
+    key            = "usecase17/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = false
