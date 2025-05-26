67232f1c36a01e883f30a9ab2382046736297e45
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-26 13:48:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,29 @@ resource "aws_subnet" "subnet_2" {
   availability_zone = "us-east-1b"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_db_subnet_group" "default" {
   name       = "pg-subnet-group"
   subnet_ids = [
