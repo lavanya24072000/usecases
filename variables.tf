0758549c2293960b5fbf4ad59048b6360240ea4b
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-27 16:55:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,66 +1,15 @@
 
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
+variable "template_url" {
+  description = "URL of the CloudFormation template"
   type        = string
-  default     = "us-west-2"
 }
 
-variable "cloudtrail_name" {
-  description = "Name of the CloudTrail"
+variable "launch_role_arn" {
+  description = "ARN of the IAM role to be used for launching the product"
   type        = string
-  default     = "my-cloudtrail"
 }
 
-variable "cloudtrail_bucket_name" {
-  description = "Name of the S3 bucket to store CloudTrail logs"
+variable "user_arn" {
+  description = "ARN of the IAM user to be granted access to the portfolio"
   type        = string
-  default     = "my-cloudtrail-bucket000011111"
-}
-
-variable "cloudwatch_log_group_name" {
-  description = "Name of the CloudWatch Logs group"
-  type        = string
-  default     = "my-cloudtrail-log-group"
-}
-
-variable "cloudtrail_role_name" {
-  description = "Name of the IAM role for CloudTrail"
-  type        = string
-  default     = "my-cloudtrail-role"
-}
-
-variable "cloudwatch_metric_filter_name" {
-  description = "Name of the CloudWatch Logs metric filter"
-  type        = string
-  default     = "console-login-filter"
-}
-
-variable "cloudwatch_metric_name" {
-  description = "Name of the CloudWatch metric"
-  type        = string
-  default     = "SuccessfulConsoleLogin"
-}
-
-variable "cloudwatch_metric_namespace" {
-  description = "Namespace of the CloudWatch metric"
-  type        = string
-  default     = "SecurityMetrics"
-}
-
-variable "cloudwatch_alarm_name" {
-  description = "Name of the CloudWatch alarm"
-  type        = string
-  default     = "console-login-alarm"
-}
-
-variable "sns_topic_name" {
-  description = "Name of the SNS topic"
-  type        = string
-  default     = "console-login-topic"
-}
-
-variable "notification_email" {
-  description = "Email address to receive notifications"
-  type        = string
-  default = "elavanya@hcltech.com"
 }
