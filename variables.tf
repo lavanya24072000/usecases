b207a26efc022d951e80015df7202273ecb74dd7
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-27 17:09:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,14 +2,17 @@
 variable "template_url" {
   description = "URL of the CloudFormation template"
   type        = string
+  default =  "https://demobucketforservicecatalog.s3.us-east-1.amazonaws.com/todaydemo/bucket-old.yaml"
 }
 
 variable "launch_role_arn" {
   description = "ARN of the IAM role to be used for launching the product"
   type        = string
+  default = "arn:aws:iam::676206899900:role/servicecatalogaccesss"
 }
 
 variable "user_arn" {
   description = "ARN of the IAM user to be granted access to the portfolio"
   type        = string
+  default = "arn:aws:iam::676206899900:user/svc"
 }
