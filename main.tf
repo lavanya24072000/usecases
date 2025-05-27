589d946c885bc2e0fdf7d50b690d49d473322b88
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-27 16:55:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,120 +1,65 @@
-provider "aws" {
-  region = var.aws_region
+
+resource "aws_servicecatalog_portfolio" "s3_portfolio" {
+  name          = "S3 Portfolio"
+  description   = "Portfolio for provisioning S3 buckets"
+  provider_name = "YourCompany"
 }
 
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "cloudtrail_bucket" {
-  bucket = var.cloudtrail_bucket_name
-}
-
-resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
-  bucket = aws_s3_bucket.cloudtrail_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AWSCloudTrailAclCheck",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action    = "s3:GetBucketAcl",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}"
-      },
-      {
-        Sid       = "AWSCloudTrailWrite",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action    = "s3:PutObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_cloudwatch_log_group" "cloudtrail_log_group" {
-  name = var.cloudwatch_log_group_name
-}
-
-resource "aws_iam_role" "cloudtrail_role" {
-  name = var.cloudtrail_role_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cloudtrail_policy_attachment" {
-  role       = aws_iam_role.cloudtrail_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-}
-
-resource "aws_cloudtrail" "trail" {
-  name                          = var.cloudtrail_name
-  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_logging                = true
-
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail_log_group.arn}:*"
-  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_role.arn
-
-  depends_on = [
-    aws_cloudwatch_log_group.cloudtrail_log_group,
-    aws_iam_role.cloudtrail_role,
-    aws_iam_role_policy_attachment.cloudtrail_policy_attachment,
-    aws_s3_bucket_policy.cloudtrail_bucket_policy
-  ]
-}
-
-resource "aws_cloudwatch_log_metric_filter" "console_login_filter" {
-  name           = "ConsoleLoginFilter"
-  log_group_name = aws_cloudwatch_log_group.cloudtrail_log_group.name
-  pattern        = "{ ($.eventName = \"ConsoleLogin\") && ($.responseElements.ConsoleLogin = \"Success\") }"
-
-  metric_transformation {
-    name      = "SuccessfulConsoleLogin"
-    namespace = "SecurityMetrics"
-    value     = "1"
+resource "aws_servicecatalog_product" "s3_product" {
+  name          = "S3 Bucket Product"
+  owner         = "YourCompany"
+  product_type  = "CLOUD_FORMATION_TEMPLATE"
+  provisioning_artifact_parameters {
+    name           = "v1"
+    type           = "CLOUD_FORMATION_TEMPLATE"
+    template_url   = var.template_url
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "console_login_alarm" {
-  alarm_name          = var.cloudwatch_alarm_name
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = aws_cloudwatch_log_metric_filter.console_login_filter.metric_transformation[0].name
-  namespace           = aws_cloudwatch_log_metric_filter.console_login_filter.metric_transformation[0].namespace
-  period              = "60"
-  statistic           = "Sum"
-  threshold           = "1"
-  alarm_actions       = [aws_sns_topic.console_login_topic.arn]
+resource "aws_servicecatalog_portfolio_product_association" "association" {
+  portfolio_id = aws_servicecatalog_portfolio.s3_portfolio.id
+  product_id   = aws_servicecatalog_product.s3_product.id
 }
 
-resource "aws_sns_topic" "console_login_topic" {
-  name = var.sns_topic_name
+resource "aws_servicecatalog_constraint" "template_constraint" {
+  portfolio_id = aws_servicecatalog_portfolio.s3_portfolio.id
+  product_id   = aws_servicecatalog_product.s3_product.id
+  type         = "TEMPLATE"
+  parameters   = jsonencode({
+    Rules = {
+      RegionRule = {
+        Assertions = [
+          {
+            Assert = "Fn::Equals([Ref(\"AWS::Region\"), \"us-east-1\"])"
+            AssertDescription = "S3 buckets must be created in us-east-1"
+          }
+        ]
+      }
+    }
+  })
 }
 
-resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.console_login_topic.arn
-  protocol  = "email"
-  endpoint  = var.notification_email
+resource "aws_servicecatalog_constraint" "launch_constraint" {
+  portfolio_id = aws_servicecatalog_portfolio.s3_portfolio.id
+  product_id   = aws_servicecatalog_product.s3_product.id
+  type         = "LAUNCH"
+  parameters   = jsonencode({
+    RoleArn = var.launch_role_arn
+  })
+}
+
+resource "aws_servicecatalog_tag_option" "env_tag" {
+  key   = "env"
+  value = "dev"
+}
+
+resource "aws_servicecatalog_tag_option_resource_association" "tag_association" {
+  resource_id = aws_servicecatalog_product.s3_product.id
+  tag_option_id = aws_servicecatalog_tag_option.env_tag.id
+}
+
+resource "aws_servicecatalog_principal_portfolio_association" "user_access" {
+  portfolio_id   = aws_servicecatalog_portfolio.s3_portfolio.id
+  principal_arn  = var.user_arn
+  principal_type = "IAM"
 }
