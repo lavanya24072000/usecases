ccb92a5f0b924763c16d199c90693b9381a6ce9c
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-27 17:39:58 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,10 @@
 
-output "cloudtrail_id" {
-  description = "The ID of the CloudTrail"
-  value       = aws_cloudtrail.trail.id
+output "portfolio_id" {
+  description = "ID of the Service Catalog portfolio"
+  value       = aws_servicecatalog_portfolio.s3_portfolio.id
 }
 
-output "cloudtrail_bucket_arn" {
-  description = "The ARN of the S3 bucket for CloudTrail logs"
-  value       = aws_s3_bucket.cloudtrail_bucket.arn
-}
-
-output "cloudwatch_log_group_arn" {
-  description = "The ARN of the CloudWatch Logs group"
-  value       = aws_cloudwatch_log_group.cloudtrail_log_group.arn
-}
-
-output "cloudtrail_role_arn" {
-  description = "The ARN of the IAM role for CloudTrail"
-  value       = aws_iam_role.cloudtrail_role.arn
-}
-
-output "cloudwatch_metric_filter_id" {
-  description = "The ID of the CloudWatch Logs metric filter"
-  value       = aws_cloudwatch_log_metric_filter.console_login_filter.id
-}
-
-output "cloudwatch_alarm_arn" {
-  description = "The ARN of the CloudWatch alarm"
-  value       = aws_cloudwatch_metric_alarm.console_login_alarm.arn
-}
-
-output "sns_topic_arn" {
-  description = "The ARN of the SNS topic"
-  value       = aws_sns_topic.console_login_topic.arn
-}
-
-output "sns_subscription_arn" {
-  description = "The ARN of the SNS topic subscription"
-  value       = aws_sns_topic_subscription.email_subscription.arn
+output "product_id" {
+  description = "ID of the Service Catalog product"
+  value       = aws_servicecatalog_product.s3_product.id
 }
